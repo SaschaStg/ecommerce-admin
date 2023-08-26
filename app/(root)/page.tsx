26bcc2911d7f93baeba690a50183ae54ba6868be
Schmidt Sascha <ss532@hdm-stuttgart.de>
2023-08-26 12:59:12 +0200

    add form and input to create store modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useStore } from "zustand";
 
 const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
     return (
         <div className="p-4">
-            <Modal title="Test" description="Test Desc" isOpen onClose={() => { }}>
-                Children
-            </Modal>
+            Root page
         </div>
     )
 }
